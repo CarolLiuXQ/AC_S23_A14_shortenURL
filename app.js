a5863518eac70f900aa574a6460ca95540f0f166
DESKTOP-N8M2BVP\s7910 <s7910565@gmail.com>
2021-07-24 23:17:44 +0800

    feat: add mongoose and modify index page

--- app.js
+++ app.js
@@ -2,15 +2,30 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 const PORT = process.env.PORT || 3000
+const bodyParser = require('body-parser')
 const URL = require('./models/URL')
 
 require('./config/mongoose')
+
+app.use(express.urlencoded({ extended: true }))
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+app.post('/', (req, res) => {
+  const originURL = req.body.URL
+  const url = new URL({
+    originURL: originURL,
+    shortenURL: 'test'
+  })
+  return url.save()
+    .then(() => res.render('index', { originURL }))
+    .catch(error => console.log(error))
+})
+
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`)
